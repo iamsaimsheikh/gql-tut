5c4446c95a8ddcdc2ad6baa651c97c4e1005292c
Mehreen <iamsaimsheikh@gmail.com>
2024-03-21 09:42:31 +0500

    add nested resolvers and types

--- src/_db.ts
+++ src/_db.ts
@@ -22,4 +22,4 @@ let reviews = [
     { id: '7', rating: 10, content: 'lorem ipsum', author_id: '3', game_id: '1' },
 ]
 
-export let db = {games,authors, reviews}
+export let db = { games, authors, reviews }
